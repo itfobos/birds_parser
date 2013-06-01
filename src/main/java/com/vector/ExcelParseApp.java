8e768d011b0731a9f50a8a5d3aae46f9d4fadb69
Fobos <tr.wolfer@gmail.com>
2013-06-01 16:36:26 +0700

    Changes for less strict file processing

--- src/main/java/com/vector/ExcelParseApp.java
+++ src/main/java/com/vector/ExcelParseApp.java
@@ -2,13 +2,31 @@ package com.vector;
 
 import com.vector.parsers.ExcelParser;
 import com.vector.utils.AppInitHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExcelParseApp {
-
     public static void main(String[] args) throws Exception {
         AppInitHelper.initApp();
+        final Logger logger = LoggerFactory.getLogger(ExcelParseApp.class);
 
         ExcelParser excelParser = AppInitHelper.getBean(ExcelParser.class);
-        excelParser.parse("D:\\Docs\\Sonya_parser\\Стандартный протокол_1.xls");
+        logger.info("File: 2007.xls");
+        excelParser.parse("D:\\Docs\\Sonya_parser\\пробы_дополнено\\пробы\\2007.xls");
+
+        logger.info("File: 2008.xls");
+        excelParser.parse("D:\\Docs\\Sonya_parser\\пробы_дополнено\\пробы\\2008.xls");
+
+        logger.info("File: 2009.xls");
+        excelParser.parse("D:\\Docs\\Sonya_parser\\пробы_дополнено\\пробы\\2009.xls");
+
+        logger.info("File: 2010.xls");
+        excelParser.parse("D:\\Docs\\Sonya_parser\\пробы_дополнено\\пробы\\2010.xls");
+
+        logger.info("File: 2011.xls");
+        excelParser.parse("D:\\Docs\\Sonya_parser\\пробы_дополнено\\пробы\\2011.xls");
+
+        logger.info("File: 2012.xls");
+        excelParser.parse("D:\\Docs\\Sonya_parser\\пробы_дополнено\\пробы\\2012.xls");
     }
 }
